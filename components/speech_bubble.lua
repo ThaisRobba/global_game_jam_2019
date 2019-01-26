a1f2f9c6f11eb045c76134459e004af77ea71584
OttoRobba <ottorobba@gamil.com>
2019-01-26 18:17:33 -0200

    Add speech_bubble component with placeholder art

---
+++ components/speech_bubble.lua
@@ -0,0 +1,23 @@
+local function speech_bubble(str)
+    local node =
+        am.translate(-150, 200) ^
+        am.scale(0, 0) ^
+            am.group {
+                am.rect(-300, -100, 300, 100),
+                am.text(str, vec4(0, 0, 0, 1))
+            }
+
+    node("scale"):action(
+        am.tween(
+            0.8,
+            {
+                scale2d = vec2(1)
+            },
+            am.ease.elastic
+        )
+    )
+
+    return node
+end
+
+return speech_bubble
