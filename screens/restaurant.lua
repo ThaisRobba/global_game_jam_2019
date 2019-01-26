5dd0e8f220d7d028fb1c58eeae36987da3a09df6
OttoRobba <ottorobba@gamil.com>
2019-01-26 18:23:19 -0200

    Restaurant now has customer

--- screens/restaurant.lua
+++ screens/restaurant.lua
@@ -1,11 +1,24 @@
+local state = require "state"
+
+------
+
 local ingredient = require "components.ingredient"
+local customer = require "components.customer"
+local speech_bubble = require "components.speech_bubble"
+
+------
 
 local item_grid_offset = vec2(-430, -150)
+local customer_offset = vec2(400, 160)
+local speech_bubble_offset = vec2(-150, 200)
+
 local function restaurant()
     local node =
         am.group {
-        am.sprite("assets/restaurant/background.jpeg"),
-        am.translate(item_grid_offset):tag("ingredients_area")
+        am.sprite("assets/restaurant/background.jpeg"):tag("background"),
+        am.translate(customer_offset):tag("character_area"),
+        am.translate(item_grid_offset):tag("ingredients_area"),
+        am.translate(speech_bubble_offset):tag("speech_bubble_area")
     }
 
     for i = 1, 10 do
@@ -22,6 +35,21 @@ local function restaurant()
 
         node("ingredients_area"):append(ingredient(i, vec2(x_offset, y_offset)))
     end
+
+    node("character_area"):append(customer("mysterious_stranger")) --this wouldn't be hardcoded for multi customers
+
+    local current_step = state.current.current_step
+
+    state:on(
+        "select_tag",
+        node,
+        function()
+            if state.current.current_step ~= current_step then
+                current_step = state.current.current_step
+            -- we want to trigger the answer speech bubble and queue the other speech bubble
+            end
+        end
+    )
 
     return node
 end
