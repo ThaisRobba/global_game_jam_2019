2ef03ff643716b10d5d0d17c8a3167e0451abb08
OttoRobba <ottorobba@gamil.com>
2019-01-26 18:16:42 -0200

    Add endgame verification

--- actions.lua
+++ actions.lua
@@ -1,6 +1,41 @@
+local function target_has_all_tags(target, tags)
+    log(table.tostring(target))
+    for key, required in pairs(target) do
+        log("checking... %s, %s", key, required)
+        if required and not tags[key] then
+            return false
+        end
+    end
+
+    return true
+end
+
 --[[ Actions receive the current state and in some cases, a payload ]]
 return {
+    set_customer = function(state, payload)
+        state.current_customer = payload
+    end,
     select_tag = function(state, payload)
-        table.insert(state.selected_tags, payload)
+        local selected_tags = state.selected_tags
+
+        table.insert(selected_tags, payload)
+
+        local current_customer, customer_tags
+        if #selected_tags >= 2 then
+            current_customer = state.current_customer
+            customer_tags = current_customer.story[state.current_step].tags
+        end
+
+        if customer_tags and target_has_all_tags(customer_tags, selected_tags) then
+            table.clear(selected_tags)
+
+            state.current_step = state.current_step + 1
+            log("New step!")
+
+            if state.current_step > 3 then
+                -- custcene or something
+                state.day_completed = true
+            end
+        end
     end
 }
